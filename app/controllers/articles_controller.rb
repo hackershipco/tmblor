b693959e27b91e92ca92526e6b3edd0f7844ab8e
Dvid Silva <dsilva@doctorondemand.com>
2014-10-25 16:27:47 -0700

    created the rest of the CRUD for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,10 +12,35 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def index
+    @articles = Article.all
+  end
+
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
 
   def show
     @article = Article.find(params[:id])
   end
+
+  def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+          redirect_to @article
+      else
+          render 'edit'
+      end
+  end
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+ 
+  redirect_to articles_path
+end
 
   private
   def article_params
